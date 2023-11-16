79013dc1da5fe29ed013f5088c4e3d9a49498457
Asjah Marie <117318989+AsjahMarie@users.noreply.github.com>
2023-11-16 11:49:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 function submitForm() {
-    console.log("submitForm function called");  // Add this line for debugging
- 
+    // Get form data
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+
+    // Prepare the email message
+    var emailMessage = "Name: " + name + "\n";
+    emailMessage += "Phone Number: " + phone + "\n";
+    emailMessage += "Email: " + email + "\n";
+    emailMessage += "Subject: " + subject + "\n\n";
+    emailMessage += "Message:\n" + message;
+
+    // Configure SMTP settings
     Email.send({
-       Host: "smtp.gmail.com",
-       Username: "asjah.marie23@gmail.com",
-       Password: "cailankk",
-       To: 'asjah.marie23@gmail.com',
-       From: document.getElementById("email").value, // Assuming you have an element with id="email"
-       Subject: "New Contact Form Inquiry",
-       Body: `
-          Name: ${document.getElementById("name").value}
-          Email: ${document.getElementById("email").value}
-          Phone: ${document.getElementById("phone").value}
-          Subject: ${document.getElementById("subject").value}
-          Message: ${document.getElementById("message").value}
-       `
+        Host: "smtp.gmail.com",
+        Username: "asjah.marie23@gmail.com",
+        Password: "Cailankk2023!!",
+        To: "asjah.marie23@gmail.com",  // Replace with your own email address
+        From: "noreply@example.com",  // Set a default "From" address
+        Subject: "New Contact Form Submission",
+        Body: emailMessage
     }).then(
-       message => alert(message)
+        message => alert("Message sent successfully!")
     );
- }
- 
+}
