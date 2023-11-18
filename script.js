6b31f1cd6089ca29bc96ffeb0f6b61137c8d1e9d
Asjah Marie <117318989+AsjahMarie@users.noreply.github.com>
2023-11-18 11:19:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+document.addEventListener("DOMContentLoaded", function () {
+    let currentSlide = 0;
+    const slides = document.querySelectorAll(".item");
+    const totalSlides = slides.length;
+    const nextButton = document.getElementById("next");
+    const prevButton = document.getElementById("prev");
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+    // Initialize the slideshow
+    showSlide(currentSlide);
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
-}
+    // Event listeners for next and previous buttons
+    nextButton.addEventListener("click", () => {
+        currentSlide = (currentSlide + 1) % totalSlides;
+        showSlide(currentSlide);
+    });
+
+    prevButton.addEventListener("click", () => {
+        currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+        showSlide(currentSlide);
+    });
+
+    // Function to display the current slide
+    function showSlide(index) {
+        slides.forEach((slide) => (slide.style.display = "none"));
+        slides[index].style.display = "block";
+    }
+});
