79df6c5aa351e5e7670457cd04e04755774c0ace
Asjah Marie <117318989+AsjahMarie@users.noreply.github.com>
2023-11-17 20:48:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
-const imageWrapper = document.querySelector('.image-wrapper')
-const imageItems = document.querySelectorAll('.image-wrapper > *')
-const imageLength = imageItems.length
-const perView = 3
- let totalScroll = 0
- const delay = 2000
+document.addEventListener("DOMContentLoaded", function () {
+  const imageWrapper = document.querySelector('.image-wrapper');
+  const imageItems = document.querySelectorAll('.image-wrapper > *');
+  const imageLength = imageItems.length;
+  const perView = 3;
+  let totalScroll = 0;
+  const delay = 2000;
 
- document.documentElement.style.setProperty('--per-view', perView);
+  // Set the total width of the image container
+  const widthEl = document.querySelector('.image-wrapper > :first-child').offsetWidth + 24;
+  imageWrapper.style.width = `${widthEl * imageLength}px`;
 
- imageWrapper.style.setProperty('--per-view', perView)
- for (let i = 0; i < perView; i++) {
-   imageWrapper.insertAdjacentHTML('beforeend', imageItems[i].outerHTML)
- }
-
- let autoScroll = setInterval(scrolling, delay)
-
- function scrolling() {
-   totalScroll++
-   if (totalScroll == imageLength + 1) {
-     clearInterval(autoScroll)
-     totalScroll = 1
-     imageWrapper.style.transition = '0s'
-    imageWrapper.style.left = '0'
-    autoScroll = setInterval(scrolling, delay)
+  // Clone the images for seamless scrolling
+  for (let i = 0; i < perView; i++) {
+    imageWrapper.insertAdjacentHTML('beforeend', imageItems[i].outerHTML);
   }
-  const widthEl = document.querySelector('.image-wrapper > :first-child').offsetWidth + 24
-  imageWrapper.style.left = `-${totalScroll * widthEl}px`
-  imageWrapper.style.transition = '.3s'
+
+  let autoScroll = setInterval(scrolling, delay);
+
+  function scrolling() {
+    totalScroll++;
+    if (totalScroll == imageLength + 1) {
+      clearInterval(autoScroll);
+      totalScroll = 1;
+      imageWrapper.style.transition = '0s';
+      imageWrapper.style.left = '0';
+      autoScroll = setInterval(scrolling, delay);
+    }
+    imageWrapper.style.left = `-${totalScroll * widthEl}px`;
+    imageWrapper.style.transition = '.3s';
+  }
+});
