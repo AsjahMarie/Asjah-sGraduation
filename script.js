98eac6aff211e5f0bd969822719761e11d4b3f6f
Asjah Marie <117318989+AsjahMarie@users.noreply.github.com>
2023-11-17 21:19:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-const imageWrapper = document.querySelector('.image-wrapper')
-const imageItems = document.querySelectorAll('.image-wrapper > *')
-const imageLength = imageItems.length
-const perView = 3
-let totalScroll = 0
-const delay = 2000
+const imageWrapper = document.querySelector('.image-wrapper');
+const imageItems = document.querySelectorAll('.image-wrapper > *');
+const imageLength = imageItems.length;
+const perView = 3;
+let totalScroll = 0;
+const delay = 2000;
 
 document.documentElement.style.setProperty('--per-view', perView);
+
+const widthEl=document.queryselector('image-wrapper > :first-child').offsetWidth + 24;
+imageWrapper.style.width = `${widthEl * imageLength}px`;
 
 imageWrapper.style.setProperty('--per-view', perView)
 for (let i = 0; i < perView; i++) {
   imageWrapper.insertAdjacentHTML('beforeend', imageItems[i].outerHTML)
 }
 
-let autoScroll = setInterval(scrolling, delay)
+let autoScroll = setInterval(scrolling, delay);
 
 function scrolling() {
-  totalScroll++
+  totalScroll++;
   if (totalScroll == imageLength + 1) {
-    clearInterval(autoScroll)
-    totalScroll = 1
-    imageWrapper.style.transition = '0s'
-    imageWrapper.style.left = '0'
-    autoScroll = setInterval(scrolling, delay)
+    clearInterval(autoScroll);
+    totalScroll = 1;
+    imageWrapper.style.transition = '0s';
+    imageWrapper.style.left = '0';
+    autoScroll = setInterval(scrolling, delay);
   }
-  const widthEl = document.querySelector('.image-wrapper > :first-child').offsetWidth + 24
   imageWrapper.style.left = `-${totalScroll * widthEl}px`
   imageWrapper.style.transition = '.3s'
-}
+});
